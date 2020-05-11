5f68f07e7e40d00c0539edc02d29cc9e13af1b8f
Piotr Dubiel <piotrdubiel@icloud.com>
2020-05-11 08:00:04 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <button @click="fetchData()">Get Products</button>
+        <ul>
+          <li v-for="item in response" :key="item.id">{{ item.translated.name }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      response: []
+    };
+  },
+  methods: {
+    fetchData() {
+      const config = { "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG" };
+      this.$http
+        .get("http://192.168.33.10/sales-channel-api/v1/product", {
+          headers: config
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.response = data.data;
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
