a74c9a72fe97dfcfe7c2dda6866c3277e356f5cc
Piotr Dubiel <piotrdubiel@icloud.com>
2020-05-13 11:26:12 +0200

    add cart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container pl-0 pr-0">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#"> <strong>Demo</strong>store </a>
+      <a class="navbar-brand" href="#">
+        <strong>Demo</strong>store
+      </a>
       <button
         class="navbar-toggler"
         type="button"
@@ -24,10 +26,14 @@
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
-              >{{ category.name }}</a
-            >
+            >{{ category.name }}</a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a v-for="subcategory in category.children" :key="subcategory.id" class="dropdown-item" href="#">{{ subcategory.name }}</a>
+              <a
+                v-for="subcategory in category.children"
+                :key="subcategory.id"
+                class="dropdown-item"
+                href="#"
+              >{{ subcategory.name }}</a>
             </div>
           </li>
         </ul>
@@ -40,21 +46,48 @@
           <button class="btn btn-success" @click="fetchProducts">Get Products</button>
           <button class="btn btn-success" @click="fetchNavigation">Fetch Navigation</button>
           <button class="btn btn-danger" @click="clear()">Clear</button>
+          <button class="btn btn-danger" @click="clearCart()">Clear Cart</button>
+          <div v-if="cart.length>0" class="cart mt-4">
+            <h1>Cart</h1>
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">Product</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="product in cart" :key="product.id">
+                  <td>{{ product.label }}</td>
+                  <td>{{ product.price.totalPrice }}$</td>
+                </tr>
+              </tbody>
+            </table>
+            <h3>Total Price: {{totalPrice}}$</h3>
+          </div>
         </div>
       </div>
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 mt-5">
         <div v-for="item in response" :key="item.id" class="col mb-4">
           <div class="card">
-            <img v-bind:src="item.cover.media.thumbnails[0].url" class="card-img-top" style="max-height: 150px;" alt="..." />
+            <img
+              v-bind:src="item.cover.media.thumbnails[0].url"
+              class="card-img-top"
+              style="max-height: 150px;"
+              alt="..."
+            />
             <div class="card-body text-center">
               <h5 class="card-title text-left" style="min-height: 80px;">
                 <router-link :to="`/product/${item.id}`">{{ item.translated.name }}</router-link>
               </h5>
-              <p class="card-text text-left" style="font-size: 12px; min-height: 110px;">{{ item.translated.description }}</p>
+              <p
+                class="card-text text-left"
+                style="font-size: 12px; min-height: 110px;"
+              >{{ item.translated.description }}</p>
               <h3 class="text-center">
                 <strong>{{ item.price[0].gross }}$</strong>
               </h3>
-              <button class="btn btn-primary">Add to shopping cart</button>
+              <button class="btn btn-primary" @click="addToCart(item.id)">Add to shopping cart</button>
             </div>
           </div>
         </div>
@@ -69,41 +102,90 @@ export default {
     return {
       response: [],
       categories: [],
+      cart: [],
+      addToCartTest: []
     };
+  },
+  computed: {
+    totalPrice: function() {
+      return this.cart.reduce((acc, val) => acc + val.price.totalPrice, 0);
+    }
   },
   methods: {
     fetchProducts() {
-      const config = { 'sw-access-key': 'SWSCZNJ1SKHBEXLUMWM1VMVPSG' };
+      const config = { "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG" };
       this.$http
-        .get('http://192.168.33.10/sales-channel-api/v1/product', {
-          headers: config,
+        .get("http://192.168.33.10/sales-channel-api/v1/product", {
+          headers: config
         })
-        .then((res) => res.json())
-        .then((data) => {
+        .then(res => res.json())
+        .then(data => {
           this.response = data.data;
-          console.log(data);
+          console.log("All products: ", data);
         });
     },
     fetchNavigation() {
-      const config = { 'sw-access-key': 'SWSCZNJ1SKHBEXLUMWM1VMVPSG' };
+      const config = { "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG" };
       this.$http
-        .get('http://192.168.33.10/store-api/v1/navigation/main-navigation/main-navigation', {
-          headers: config,
-        })
-        .then((res) => res.json())
-        .then((data) => {
+        .get(
+          "http://192.168.33.10/store-api/v1/navigation/main-navigation/main-navigation",
+          {
+            headers: config
+          }
+        )
+        .then(res => res.json())
+        .then(data => {
           this.categories = data;
-          console.log('Navigation: ', this.categories);
+          console.log("Navigation: ", this.categories);
+        });
+    },
+    fetchCart() {
+      const config = { "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG" };
+      this.$http
+        .get("http://192.168.33.10/sales-channel-api/v1/checkout/cart", {
+          headers: config
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.cart = data.data;
+          console.log("Cart: ", data);
+        });
+    },
+    printProductId(id) {
+      console.log("Product ID: ", id);
+    },
+    addToCart(id) {
+      const config = {
+        "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG"
+      };
+      this.$http
+        .post(
+          `http://192.168.33.10/sales-channel-api/v1/checkout/cart/product/${id}`,
+          null,
+          {
+            headers: config
+          }
+        )
+        .then(res => res.json())
+        .then(data => {
+          //this.fetchCart();
+          this.cart.push(data.data.lineItems[0]);
+          console.log("Item data: ", data.data.lineItems[0]);
+          console.log("Cart: ", this.cart);
         });
     },
     clear() {
       this.response = [];
       this.categories = [];
     },
+    clearCart() {
+      this.cart = [];
+    }
   },
   created() {
     this.fetchNavigation();
     this.fetchProducts();
-  },
+    //this.fetchCart();
+  }
 };
 </script>
--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -4,33 +4,46 @@
     <h1>Product</h1>
     <h2>Stock: {{ productData.stock }}</h2>
     <h6>Product ID: {{ id }}</h6>
-    <img v-bind:src="productData.cover.media.thumbnails[0].url" alt="Product image" style="max-height: 200px;" />
-    <h6>Product Name: {{ productData.translated.name }}</h6>
+    <img v-bind:src="productData.cover.media.url" alt="Product image" style="max-height: 200px;" />
+    <h6 class="mt-4">Product Name: {{ productData.translated.name }}</h6>
     <h6>Product Description: {{ productData.translated.description }}</h6>
+    <h2 class="mt-5">Properties: {{ productData.sortedProperties }}</h2>
     <table class="table table-sm mt-5 col-lg-6">
       <tbody>
         <tr>
-          <td><strong>color</strong></td>
+          <td>
+            <strong>color</strong>
+          </td>
           <td>@mdo</td>
         </tr>
         <tr>
-          <td><strong>content</strong></td>
+          <td>
+            <strong>content</strong>
+          </td>
           <td>@fat</td>
         </tr>
         <tr>
-          <td><strong>length</strong></td>
+          <td>
+            <strong>length</strong>
+          </td>
           <td>@twitter</td>
         </tr>
         <tr>
-          <td><strong>size</strong></td>
+          <td>
+            <strong>size</strong>
+          </td>
           <td>@twitter</td>
         </tr>
         <tr>
-          <td><strong>textile</strong></td>
+          <td>
+            <strong>textile</strong>
+          </td>
           <td>@twitter</td>
         </tr>
         <tr>
-          <td><strong>width</strong></td>
+          <td>
+            <strong>width</strong>
+          </td>
           <td>@twitter</td>
         </tr>
       </tbody>
@@ -43,26 +56,26 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-      productData: [],
+      productData: []
     };
   },
   methods: {
     fetchProduct() {
-      const config = { 'sw-access-key': 'SWSCZNJ1SKHBEXLUMWM1VMVPSG' };
+      const config = { "sw-access-key": "SWSCZNJ1SKHBEXLUMWM1VMVPSG" };
       this.$http
         .get(`http://192.168.33.10/sales-channel-api/v1/product/${this.id}`, {
-          headers: config,
+          headers: config
         })
-        .then((res) => res.json())
-        .then((data) => {
+        .then(res => res.json())
+        .then(data => {
           this.productData = data.data;
-          console.log('Single Product Data: ', data);
+          console.log("Single Product Data: ", data);
         })
-        .catch((err) => console.log(err));
-    },
+        .catch(err => console.log(err));
+    }
   },
-  created() {
+  mounted() {
     this.fetchProduct();
-  },
+  }
 };
 </script>
